1e052e85364f5fcbb2a69b1164f6e32a94b31344
adityapaul26 <adityapaul2603@gmail.com>
2026-05-31 11:00:20 +0530

    ...

--- components/ui/magic-bento.jsx
+++ components/ui/magic-bento.jsx
@@ -155,7 +155,7 @@ const cardData = [
     background: "linear-gradient(135deg, #0f172a 0%, #120f17 100%)",
     title: "Workspace Tools",
     description: (
-      <div className="mt-2  h-58" style={{ clipPath: "inset(0 0 20% 0)" }}>
+      <div className="mt-2  h-80" style={{ clipPath: "inset(0 0 10% 0)" }}>
         <OrbitingCircles radius={80} path={false} className="h-full w-full">
           <img src="https://skillicons.dev/icons?i=linux" className="h-8 w-8" />
           <img
@@ -735,7 +735,7 @@ const GlobalSpotlight = ({
 
 const BentoCardGrid = ({ children, gridRef }) => (
   <div
-    className="bento-section grid gap-2 p-3 max-w-[64rem] w-full select-none relative mx-auto"
+    className="bento-section grid gap-4 p-3 max-w-7xl w-full select-none relative mx-auto"
     style={{ fontSize: "clamp(1rem, 0.9rem + 0.5vw, 1.5rem)" }}
     ref={gridRef}
   >
@@ -933,9 +933,9 @@ const MagicBento = ({
       )}
 
       <BentoCardGrid gridRef={gridRef}>
-        <div className="card-responsive grid gap-2">
+        <div className="card-responsive grid gap-4">
           {cardData.map((card, index) => {
-            const baseClassName = `card flex flex-col justify-between relative aspect-[4/3] min-h-[240px] w-full max-w-full p-5 rounded-[20px] border border-solid font-light overflow-hidden transition-colors duration-300 ease-in-out hover:-translate-y-0.5 hover:shadow-[0_8px_25px_rgba(0,0,0,0.15)] ${
+            const baseClassName = `card flex flex-col justify-between relative aspect-[4/3] min-h-[300px] w-full max-w-full p-6 rounded-[24px] border border-solid font-light overflow-hidden transition-colors duration-300 ease-in-out hover:-translate-y-0.5 hover:shadow-[0_8px_25px_rgba(0,0,0,0.15)] ${
               enableBorderGlow ? "card--border-glow" : ""
             }`;
 
